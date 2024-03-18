4839a1e6621fffc9f877f15c69d581db75334d5a
Kelvin Rugut <krugut36@gmail.com>
2024-03-18 15:47:03 +0300

    Changes for bulk_create

--- apiApplication/views.py
+++ apiApplication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db import transaction
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,28 +20,30 @@ def create_products(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# bulk_create for products only
-# @api_view(['POST'])
-# def create_products_bulk(request):
-#     products_data = request.data
-#     # Create a list of Product objects from the JSON data
-#     products = [Product(name=item['name'], image=item.get('image', '')) for item in products_data]
-#     Product.objects.bulk_create(products)
-#     return Response("Products created successfully", status=status.HTTP_201_CREATED)
-
+# bulk_create for products and product_variants
 @api_view(['POST'])
 def create_products_bulk(request):
     products_data = request.data
-    products = [Product(name=item['name'], image=item.get('image', '')) for item in products_data]
-    # Bulk create products
-    Product.objects.bulk_create(products)
-    
-    # Create variants if provided
-    for product, product_data in zip(products, products_data):
+    products_to_create = []
+    variants_to_create = []
+
+    for product_data in products_data:
+        product = Product(name=product_data['name'], image=product_data.get('image', ''))
+        products_to_create.append(product)
+
         variants_data = product_data.get('variants')
         if variants_data:
-            product_variants = [Product_variant(product_id=product, **variant) for variant in variants_data]
-            Product_variant.objects.bulk_create(product_variants)
+            for variant_data in variants_data:
+                variant = Product_variant(product_id=product, **variant_data)
+                variants_to_create.append(variant)
+
+    with transaction.atomic():
+        Product.objects.bulk_create(products_to_create)
+        batch_size = max(len(variants_to_create) // 10, 1)
+        for i in range(0, len(variants_to_create), batch_size):
+            batch = variants_to_create[i:i+batch_size]
+            Product_variant.objects.bulk_create(batch)
+
     return Response("Products and variants created successfully", status=status.HTTP_201_CREATED)
 
 # listing products
